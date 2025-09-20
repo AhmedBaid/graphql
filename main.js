7bd8a04350d17834edb6d5341a5cf8f79eae2bb4
AhmedBaid <baidahmed2005@gmail.com>
2025-09-20 18:14:57 +0100

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import { container, FormLogin } from "./config/config.js";
-
 container.innerHTML=FormLogin
+async function main() {
+    const username = document.getElementById("username")
+    const password = document.getElementById("password")
+    const response = await fetch("https://learn.zone01oujda.ma/api/auth/signin",)
+
+}
+main()
